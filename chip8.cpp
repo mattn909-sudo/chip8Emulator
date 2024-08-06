2a8c52a783b04a54d922583959ea52a70bba52af
Matt Nguyen <mattn9091@gmail.com>
2024-08-06 12:36:32 -0400

    Add initialize function

--- chip8.cpp
+++ chip8.cpp
@@ -1,6 +1,29 @@
 #include "chip8.h"
 
 void chip8::initialize(){
+    pc = 0x200;
+    opcode = 0;
+    I = 0;
+    sp = 0;
+
+    for(int i = 0; i < 4096;i++){
+        memory[i] = 0;
+    }
+
+    for(int i = 0; i < 16;i++){
+        stack[i] = 0;
+        V[i] = 0;
+    }
+
+    for(int i = 0; i < (64*32); i++){
+        gfx[i] = 0;
+    }
+
+    for(int i = 0; i < 80; i++){
+        memory[i] = chip8_fontset[i];
+    }
+    
+
 
 }
 
