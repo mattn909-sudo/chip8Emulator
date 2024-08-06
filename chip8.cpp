9cd9fa47fcfcab224e5122ad741cd9bb7d4ff2b4
Matt Nguyen <mattn9091@gmail.com>
2024-08-06 18:30:08 -0400

    Implement opcodes to get IBM logo test running

--- chip8.cpp
+++ chip8.cpp
@@ -22,19 +22,19 @@ void chip8::initialize(){
     for(int i = 0; i < 80; i++){
         memory[i] = chip8_fontset[i];
     }
-    
-
-
 }
 
 void chip8::emulateCycle(){
     opcode = memory[pc] << 8 | memory[pc + 1];
-
+    pc += 2;
     switch(opcode & 0xF000){
         case 0x0000:
             switch(opcode & 0x000F){
                 case 0x0000:
                     //clear screen
+                    for(int i = 0; i < (64*32); i++){
+                        gfx[i] = 0;
+                    }
                     break;
                 case 0x00E:
                     //return
@@ -42,11 +42,15 @@ void chip8::emulateCycle(){
             }
         case 0x1000:
             //1nnn
-            //goto address 1-NNN
+            //goto address NNN
+            pc = opcode & 0x0FFF;
             break;
         case 0x2000:
             //2nnn
             //calls sub routine at NNN
+            stack[sp] = pc;
+            ++sp;
+            pc = opcode & 0x0FFF;
             break;
         case 0x3000:
             //3xnn
@@ -60,9 +64,11 @@ void chip8::emulateCycle(){
             break;
         case 0x6000:
             //6xnn vx = nn
+            V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
             break;
         case 0x7000:
             //7xnn vx += nn
+            V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
         case 0x8000:
             switch (opcode & 0x000F){
                 case 0x0000:
@@ -79,9 +85,24 @@ void chip8::emulateCycle(){
                     break;
                 case 0x0004:
                     //8xy4 vx += vy
+                    if(V[(opcode & 0x00F0)>> 4] > (0xFF - V[(opcode & 0x0F00) >> 8])){
+                        V[0xF] = 1;
+                    }
+                    else{
+                        V[0xF] = 0;
+                        V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0)>> 4];
+                    }
                     break;
                 case 0x0005:
                     //8xy5 vx -= vy
+                    if(V[(opcode & 0x00F0)>> 4] >= (V[(opcode & 0x0F00) >> 8])){
+                        V[0xF] = 0;
+                    }
+                    else{
+                        V[0xF] = 1;
+                        V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0)>> 4];
+                        pc += 2;
+                    }
                     break;
                 case 0x0006:
                     //8xy6 vx >>= vy
@@ -108,6 +129,22 @@ void chip8::emulateCycle(){
             break;
         case 0xD000:
             //dxyn draw(vx,vy,n)
+            const char x = ((opcode & 0x0F00) >> 8) & 63;
+            const char y = ((opcode & 0x00F0) >> 4) & 31;
+            const char height = opcode & 0x000F;
+            unsigned short pixel;
+            V[0xF] = 0;
+
+            for(int yLine = 0; yLine > height; yLine++){
+                pixel = memory[I + yLine];
+                for(int xLine = 0;xLine < 8; xLine++){
+                    if(gfx[(x + xLine + ((y + yLine) * 64))] == 1) V[0xF] = 1; 
+                                                                                                   
+                    gfx[x + xLine + ((y + yLine) * 64)] ^= 1;
+                }
+            }
+
+            drawFlag = true;
             break;
         case 0xE000:
             switch (opcode & 0x00FF){
